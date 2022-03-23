06baf1157de93313c2ffbd6d78650d40c3a217e6
unknown <oqzcmcuxr@mozmail.com>
2022-03-23 16:25:12 +0300

    feat: aka page

--- src/views/Aka.tsx
+++ src/views/Aka.tsx
@@ -1,5 +1,19 @@
+import React from "react";
+
 function Aka() {
-  return <></>;
+  return (
+    <div className="mt-32 mx-12">
+      <div className="p-4 rounded bg-[#313131]">
+        <input
+          type="text"
+          className="w-full p-2 text-3xl bg-transparent rounded border border-[#6153CC] outline-none"
+        />
+        <button className="bg-[#6153CC] mt-4 w-full p-2 rounded text-2xl hover:brightness-125 active:brightness-100">
+          tiny it!
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Aka;
