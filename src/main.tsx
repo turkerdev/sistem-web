44b09e4a419974258e67811f907eca9d410364cd
unknown <oqzcmcuxr@mozmail.com>
2022-03-23 08:42:55 +0300

    feat: react query

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,21 @@ import "./index.css";
 import App from "./App";
 import Aka from "./views/Aka";
 import NotFound from "./views/NotFound";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/aka" element={<Aka />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/aka" element={<Aka />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
